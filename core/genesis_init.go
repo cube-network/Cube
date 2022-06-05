87253eb6c8462cb2855eddd6995078601b655a61
zzyalbert <zzy.albert@163.com>
2022-06-05 13:09:19 +0800

    genesis: update chainconfig for different rewards plan

--- core/genesis_init.go
+++ core/genesis_init.go
@@ -39,8 +39,8 @@ const (
 )
 
 var (
-	// Rewards plan of version 1
-	RewardsByMonth1 = []*big.Int{
+	// Rewards plan of rule 1
+	rewardsByMonth1 = []*big.Int{
 		fromGwei(460590277777778), fromGwei(516685474537037), fromGwei(573248131269290), fromGwei(630282143474312), fromGwei(687791439114376), fromGwei(745779978884773),
 		fromGwei(838973978708813), fromGwei(932944595198053), fromGwei(1027698300158040), fromGwei(1123241619326020), fromGwei(1219581132820400), fromGwei(1316723475593910),
 		fromGwei(1449397560112750), fromGwei(1583177262002570), fromGwei(1718071794741480), fromGwei(1854090448586550), fromGwei(1991242591213660), fromGwei(2129537668362660),
@@ -51,8 +51,8 @@ var (
 		fromGwei(6276633654864210), fromGwei(6415918101988080), fromGwei(6556363252837980), fromGwei(6697978779944960), fromGwei(6840774436444510), fromGwei(6984760056748220),
 	}
 
-	// Default rewards plan, which is version 2
-	RewardsByMonth = []*big.Int{
+	// Default rewards plan, which is rule 2
+	rewardsByMonth2 = []*big.Int{
 		fromGwei(384027777800000), fromGwei(446255787000000), fromGwei(509002363000000), fromGwei(572271827200000), fromGwei(636068536800000), fromGwei(700396885800000),
 		fromGwei(799983526500000), fromGwei(900400055900000), fromGwei(1001653390000000), fromGwei(1095417168000000), fromGwei(1189962311000000), fromGwei(1285295330000000),
 		fromGwei(1416145014000000), fromGwei(1548085111000000), fromGwei(1681124709000000), fromGwei(1815272970000000), fromGwei(1950539134000000), fromGwei(2086932516000000),
@@ -63,15 +63,30 @@ var (
 		fromGwei(6180975254000000), fromGwei(6317900048000000), fromGwei(6455965882000000), fromGwei(6595182264000000), fromGwei(6735558783000000), fromGwei(6877105106000000),
 	}
 
-	rewardsPlans = map[uint64][]*big.Int{
-		1: RewardsByMonth1,
-		2: RewardsByMonth,
+	rewardsPlans = [][]*big.Int{
+		rewardsByMonth1,
+		rewardsByMonth2,
 	}
 )
 
 // fromGwei convert amount from gwei to wei
 func fromGwei(gwei int64) *big.Int {
 	return new(big.Int).Mul(big.NewInt(gwei), big.NewInt(1000000000))
+}
+
+// RewardsByMonth returns RewardsByMonth info by different rules
+// rule begins from 1, and 0 will return default value
+func RewardsByMonth(rule uint64) []*big.Int {
+	var idx int
+	if rule == 0 { // 0 is the latest rule by default
+		idx = len(rewardsPlans) - 1
+	} else {
+		idx = int(rule) - 1
+	}
+	if idx >= len(rewardsPlans) {
+		panic(fmt.Sprintf("Unknown chaos rewards rule: %v\n", rule))
+	}
+	return rewardsPlans[idx]
 }
 
 // genesisInit is tools to init system contracts in genesis
@@ -129,13 +144,8 @@ func (env *genesisInit) initStaking() error {
 	for _, validator := range env.genesis.Validators {
 		totalValidatorStake = new(big.Int).Add(totalValidatorStake, new(big.Int).Mul(validator.Stake, big.NewInt(1000000000000000000)))
 	}
-	rewardsByMonth := RewardsByMonth
-	// We use default plan when contract.Init.RewardsVer is nil or 0
-	if contract.Init.RewardsVer != nil && contract.Init.RewardsVer.Uint64() != 0 {
-		if rewardsByMonth, ok = rewardsPlans[contract.Init.RewardsVer.Uint64()]; !ok {
-			return errors.New(fmt.Sprintf("Unknown RewardsVer: %v", contract.Init.RewardsVer.Uint64()))
-		}
-	}
+	rewardsByMonth := RewardsByMonth(env.genesis.Config.Chaos.Rule)
+
 	totalRewards := big.NewInt(0)
 	for _, rewards := range rewardsByMonth {
 		totalRewards = new(big.Int).Add(totalRewards, rewards)

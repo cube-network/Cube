b2f7b1b21b80e5d569b40bec3a4766c703138e8d
zzyalbert <zzy.albert@163.com>
2022-07-01 10:42:11 +0800

    release internal version

--- params/version.go
+++ params/version.go
@@ -24,7 +24,7 @@ const (
 	VersionMajor = 1        // Major version component of the current release
 	VersionMinor = 1        // Minor version component of the current release
 	VersionPatch = 0        // Patch version component of the current release
-	VersionMeta  = "stable" // Version metadata to append to the version string
+	VersionMeta  = "internal-stable" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.

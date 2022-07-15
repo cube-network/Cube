3fe2c22efecada9087911321bb45436eec547402
MinZhuC <zhumin02@global-hgroup.com>
2022-07-15 17:03:54 +0800

    update contract's abi in genesis configuration

--- crosschain/app.go
+++ crosschain/app.go
@@ -255,10 +255,10 @@ func (app *CosmosApp) setupSDKModule(skipUpgradeHeights map[int64]bool, homePath
 	// )
 
 	// todo:
-	feecollectorAcc, _ := sdk.AccAddressFromHex("0x1223456")
-	feeibcAcc, _ := sdk.AccAddressFromHex("0x1223456")
-	transferAcc, _ := sdk.AccAddressFromHex("0x1223456")
-	mintAcc, _ := sdk.AccAddressFromHex("0x1223456")
+	feecollectorAcc, _ := sdk.AccAddressFromHex("0x39A21562232Fd6184947a62ce707F5a56F19Fa55")
+	feeibcAcc, _ := sdk.AccAddressFromHex("0x39A21562232Fd6184947a62ce707F5a56F19Fa55")
+	transferAcc, _ := sdk.AccAddressFromHex("0x39A21562232Fd6184947a62ce707F5a56F19Fa55")
+	mintAcc, _ := sdk.AccAddressFromHex("0x39A21562232Fd6184947a62ce707F5a56F19Fa55")
 	moduleAccs := map[string]sdk.AccAddress{
 		"fee_collector": feecollectorAcc,
 		"feeibc":        feeibcAcc,

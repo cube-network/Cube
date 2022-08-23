59b53059d89ddd62ed80d9b93d3e98a348ffef97
ethatbtc <ethatbtc@gmail.com>
2022-08-23 14:57:09 +0800

    refactor hashcache in trie to resolve unexpected missing trie ndoe

--- core/state/database.go
+++ core/state/database.go
@@ -103,6 +103,9 @@ type Trie interface {
 	// nodes of the longest existing prefix of the key (at least the root), ending
 	// with the node that proves the absence of the key.
 	Prove(key []byte, fromLevel uint, proofDb ethdb.KeyValueWriter) error
+
+	// TrieNodeHashCache returns internal hash cache of trie node
+	TrieNodeHashCache() *trie.HashCache
 }
 
 // NewDatabase creates a backing store for state. The returned database is safe for
